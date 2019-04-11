be8e5132e4c86ae0e752e46088c4e58816a74b1e
Matthew <m.humphs@gmail.com>
2019-04-11 23:06:52 +0100

    Another failed attempt

--- setup.py
+++ setup.py
@@ -5,17 +5,18 @@ with open('README.md','r') as fh:
 
 setuptools.setup(
     name         = 'mvdh',
-    version      = '0.1.4',
+    version      = '0.1.5',
     author       = 'Matthew P. Humphreys',
     author_email = 'm.p.humphreys@cantab.net',
     description  = 'Miscellaneous Python tools',
     url          = 'https://github.com/mvdh7/mvdh',
     packages     = setuptools.find_packages(),
-    install_requires = ['autograd==1.2'],
-    dependency_links = ['https://github.com/mvdh7/autograd/tarball/master'],
+    install_requires = ['autograd'],
+    dependency_links = ['git+https://github.com/mvdh7/autograd#egg=autograd'],
     long_description = long_description,
     long_description_content_type = 'text/markdown',
     classifiers = (
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',),)
+#tarball/master
